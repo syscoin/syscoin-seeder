ab5ee8c6ab27f0d34ee45d26b406fc132895bd60
sidhujag <jsidhu@blockchainfoundry.co>
2019-12-23 18:49:56 -0800

    add subversion check and updated min height

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -298,9 +298,13 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
         ret = 0;
         ban = 1;
         // TODO: update this as checkpoints
-      } else if(blocks < 292956){
+      } else if(blocks < 297723){
         ret = 0;
         ban = 1;
+      }
+      else if(clientSV.find("/Satoshi:4.1.0/") == clientSV.end()){
+        ret = 0;
+        ban = 1;     
       }
       else {
         ban = 0;
