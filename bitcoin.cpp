dc00e0cd51e53e7971fd46e6aa42dbbcc4c44c78
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-09-24 17:05:07 -0700

    4.3.0rc1

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -319,13 +319,18 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
       if (clientV < MIN_VERSION) {
         ret = 0;
         ban = 1;
-      } else if(blocks < 1004200){
+      } 
+      else if(clientSV.find("/Satoshi:4.3.0/") == std::string::npos && clientSV.find("/Satoshi:4.3.0rc1/") == std::string::npos){
+        ret = 0;
+        ban = 1;     
+      }
+      else if(clientSV.find("/Satoshi:4.3.0rc1/") != std::string::npos && blocks < 840000){
         ret = 0;
         ban = 1;
       }
-      else if(clientSV.find("/Satoshi:4.2.1/") == std::string::npos && clientSV.find("/Satoshi:4.2.2/") == std::string::npos){
+      else if(clientSV.find("/Satoshi:4.3.0/") != std::string::npos && blocks < 1348000){
         ret = 0;
-        ban = 1;     
+        ban = 1;
       }
       else {
         ban = 0;
