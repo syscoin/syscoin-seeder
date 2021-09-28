a2d235f8da6058d40aa0d475d1e64e3dd4f446ea
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-09-28 16:07:53 -0700

    only use 4.3.0 in seeder

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -320,15 +320,11 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
         ret = 0;
         ban = 1;
       } 
-      else if(clientSV.find("/Satoshi:4.3.0/") == std::string::npos && clientSV.find("/Satoshi:4.3.0rc2/") == std::string::npos){
+      else if(clientSV.find("/Satoshi:4.3.0/") == std::string::npos){
         ret = 0;
         ban = 1;     
       }
-      else if(clientSV.find("/Satoshi:4.3.0rc2/") != std::string::npos && blocks < 840000){
-        ret = 0;
-        ban = 1;
-      }
-      else if(clientSV.find("/Satoshi:4.3.0/") != std::string::npos && blocks < 1348000){
+      else if(clientSV.find("/Satoshi:4.3.0/") != std::string::npos && blocks < 840000){
         ret = 0;
         ban = 1;
       }
